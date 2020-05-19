4991990804e7cfbc179ac21f6a4878edf5fb9525
Doni Setiawan <donisetiawan.050@gmail.com>
2020-05-19 12:29:40 +0700

    Adding <td> Content

--- src/Excel.jsx
+++ src/Excel.jsx
@@ -1,22 +1,46 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-export default function Excel(props) {
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            {props.headers.map((title) => (
-              <th key={title}>{title}</th>
+class Excel extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: this.props.initialData,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              {this.props.headers.map((title) => (
+                <th key={title}>{title}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map((row) => (
+              <tr key={row[3]}>
+                {row.map((cell) => (
+                  <td key={cell}>{cell}</td>
+                ))}
+              </tr>
             ))}
-          </tr>
-        </thead>
-      </table>
-    </div>
-  );
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
+
+export default Excel;
 
 Excel.propTypes = {
   headers: PropTypes.arrayOf(PropTypes.string).isRequired,
+  initialData: PropTypes.arrayOf(
+    PropTypes.arrayOf(PropTypes.string),
+  ).isRequired,
 };
